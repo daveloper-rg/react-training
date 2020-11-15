c42a5e7900b909ff6caa286fa833dfb88f4bfa41
Ramirez David <david.ramirez@tui.be>
2020-11-15 18:12:24 +0100

    Fetch api and install use-debounce

--- 03-hooks/01-use-state/src/demo.tsx
+++ 03-hooks/01-use-state/src/demo.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 
-export const MyComponent: React.FC = () => {
-  const [myName, setMyName] = React.useState("John Doe");
+export const MyComponent = () => {
+  const [filter, setFilter] = React.useState("");
+  const [userCollection, setUserCollection] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/?name=${filter}`)
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.results) {
+          setUserCollection(json.results);
+        } else {
+          setUserCollection([]);
+        }
+      });
+  }, [filter]);
 
   return (
-    <>
-      <h4>{myName}</h4>
-      <input value={myName} onChange={(e) => setMyName(e.target.value)} />
-    </>
+    <div>
+      <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+      <ul>
+        {userCollection.map((user, index) => (
+          <li key={index}>
+            <img src={user.image} width="50px" />
+            {user.name}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
+// instalamos debounce con: npm install use-debounce --save
