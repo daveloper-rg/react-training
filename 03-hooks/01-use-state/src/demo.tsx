73ebdf50a16eae8978a2ef8f43a4652ba4b64ee9
Ramirez David <david.ramirez@tui.be>
2020-11-15 16:46:49 +0100

    Hook useState useInfo interface

--- 03-hooks/01-use-state/src/demo.tsx
+++ 03-hooks/01-use-state/src/demo.tsx
@@ -1,12 +1,39 @@
 import React from "react";
 
+interface UserInfo {
+  name: string;
+  lastname: string;
+}
 export const MyComponent: React.FC = () => {
-  const [myName, setMyName] = React.useState("John Doe");
+  const [userInfo, setUserInfo] = React.useState<UserInfo>({
+    name: "John",
+    lastname: "Doe",
+  });
 
   return (
     <>
-      <h4>{myName}</h4>
-      <input value={myName} onChange={(e) => setMyName(e.target.value)} />
+      <h4>
+        {userInfo.name} {userInfo.lastname}
+      </h4>
+      <input
+        value={userInfo.name}
+        onChange={(e) =>
+          setUserInfo({
+            ...userInfo,
+            name: e.target.value,
+          })
+        }
+      />
+
+      <input
+        value={userInfo.lastname}
+        onChange={(e) =>
+          setUserInfo({
+            ...userInfo,
+            lastname: e.target.value,
+          })
+        }
+      />
     </>
   );
 };
