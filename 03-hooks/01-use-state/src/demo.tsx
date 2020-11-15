51ab43b1857b96c946a5570915f7902f1d0577a1
Ramirez David <david.ramirez@tui.be>
2020-11-15 17:48:02 +0100

    Visible toggle with childcomponent

--- 03-hooks/01-use-state/src/demo.tsx
+++ 03-hooks/01-use-state/src/demo.tsx
@@ -1,15 +1,5 @@
 import React from "react";
 
-export const MyChildComponent = () => {
-  React.useEffect(() => {
-    console.log("El componente se acaba de montar en el dom");
-
-    return () => console.log("** ME DESMONTAN !!***");
-  }, []);
-
-  return <h4>Hello from Child component</h4>;
-};
-
 export const MyComponent: React.FC = () => {
   const [visible, setVisible] = React.useState(false);
 
@@ -19,6 +9,36 @@ export const MyComponent: React.FC = () => {
       <button onClick={() => setVisible(!visible)}>
         Toggle Child component visibility
       </button>
+    </>
+  );
+};
+
+export const MyChildComponent = () => {
+  const [userInfo, setUserInfo] = React.useState({
+    name: "John",
+    lastname: "Doe",
+  });
+
+  React.useEffect(() => {
+    console.log("A. Call right after every render");
+
+    return () => {
+      console.log("B. Cleanup function called right before next render");
+    };
+  }, [userInfo.lastname]);
+  return (
+    <>
+      <h3>
+        {userInfo.name} {userInfo.lastname}
+      </h3>
+      <input
+        value={userInfo.name}
+        onChange={(e) => setUserInfo({ ...userInfo, name: e.target.value })}
+      ></input>
+      <input
+        value={userInfo.lastname}
+        onChange={(e) => setUserInfo({ ...userInfo, lastname: e.target.value })}
+      ></input>
     </>
   );
 };
