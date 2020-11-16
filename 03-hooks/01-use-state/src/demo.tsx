6d88abc9a3a709de9e1285182b3571941c0d9b5b
Ramirez David <david.ramirez@tui.be>
2020-11-16 09:29:02 +0100

    async await with regenerator runtime

--- 03-hooks/01-use-state/src/demo.tsx
+++ 03-hooks/01-use-state/src/demo.tsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { useDebounce } from "use-debounce";
 
-export const MyComponent: React.FC = () => {
-  const [myName, setMyName] = React.useState("John Doe");
+export const MyComponent = () => {
+  const [filter, setFilter] = React.useState("");
+  const [debouncedFilter] = useDebounce(filter, 500);
+  const [userCollection, setUserCollection] = React.useState([]);
 
-  return (
-    <>
-      <h4>{myName}</h4>
-      <input value={myName} onChange={(e) => setMyName(e.target.value)} />
-    </>
-  );
+  const retreiveData = async (filter) => {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/?name=${filter}`
+    );
+    const json = await response.json();
+
+    if (json.results) {
+      setUserCollection(json.results);
+    } else {
+      setUserCollection([]);
+    }
+  };
+
+  React.useEffect(() => {
+    retreiveData(filter);
+  }, [debouncedFilter]);
+
 };
+// instalamos regenerator-runtime: npm install regenerator-runtime --save
+// add "babel-regenerator-runtime" en webpack-config.js
