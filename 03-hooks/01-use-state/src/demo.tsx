e9495ebcabe3deb3a6c0795f20abe99c48885fe3
Ramirez David <david.ramirez@tui.be>
2020-11-16 11:26:28 +0100

    Componente puro con React.Memo

--- 03-hooks/01-use-state/src/demo.tsx
+++ 03-hooks/01-use-state/src/demo.tsx
@@ -1,12 +1,43 @@
 import React from "react";
 
-export const MyComponent: React.FC = () => {
-  const [myName, setMyName] = React.useState("John Doe");
+export const MyComponent = () => {
+  const [userInfo, setUserInfo] = React.useState({
+    name: "John",
+    lastname: "Doe",
+  });
 
   return (
     <>
-      <h4>{myName}</h4>
-      <input value={myName} onChange={(e) => setMyName(e.target.value)} />
+      <DisplayUsername name={userInfo.name} />
+      <input
+        value={userInfo.name}
+        onChange={(e) =>
+          setUserInfo({
+            ...userInfo,
+            name: e.target.value,
+          })
+        }
+      />
+      <input
+        value={userInfo.lastname}
+        onChange={(e) =>
+          setUserInfo({
+            ...userInfo,
+            lastname: e.target.value,
+          })
+        }
+      />
     </>
   );
 };
+interface Props {
+  name: string;
+}
+// con ReactMemo si hacemos un cambio en lastname no se ejecuta DisplayUsername
+export const DisplayUsername = React.memo((props: Props) => {
+  console.log(
+    "Hey I'm only rendered the first time, check React.memo + callback"
+  );
+
+  return <h3>{props.name}</h3>;
+});
