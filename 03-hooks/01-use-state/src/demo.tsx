82cce810dc198178ecdb76fcbc801e0e94ad30b3
Ramirez David <david.ramirez@tui.be>
2020-11-16 15:22:28 +0100

    Example Use Ref

--- 03-hooks/01-use-state/src/demo.tsx
+++ 03-hooks/01-use-state/src/demo.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 
+export const useWithRef = function <T>(initialValue: T) {
+  const [state, setInternalState] = React.useState(initialValue);
+  const ref = React.useRef(initialValue);
+
+  const setState = (value: T) => {
+    ref.current = value;
+    setInternalState(value);
+  };
+
+  return [state, setState, ref] as const;
+};
+
 export const MyComponent: React.FC = () => {
-  const [myName, setMyName] = React.useState("John Doe");
+  const [message, setMessage] = React.useState("initial message");
+  const [seconds, setSeconds, secondsRef] = useWithRef(0);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      console.log(seconds);
+      setSeconds(1);
+    }, 1000);
+
+    setTimeout(() => {
+      setMessage(`Total seconds: ${secondsRef.current}`);
+    }, 2000);
+  }, []);
 
   return (
     <>
-      <h4>{myName}</h4>
-      <input value={myName} onChange={(e) => setMyName(e.target.value)} />
+      <h3>{message}</h3>
+      <h4>{seconds}</h4>
     </>
   );
 };
