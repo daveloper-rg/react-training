1e4e9808ba91dc7121817719169a0d8d8f8bc146
Ramirez David <david.ramirez@tui.be>
2020-11-16 15:03:33 +0100

    hook reduce

--- 03-hooks/01-use-state/src/demo.tsx
+++ 03-hooks/01-use-state/src/demo.tsx
@@ -1,12 +1,95 @@
 import React from "react";
 
-export const MyComponent: React.FC = () => {
-  const [myName, setMyName] = React.useState("John Doe");
+interface UserState {
+  name: string,
+  lastname: string;
+}
+
+interface Action {
+  type: string;
+  payload: any;
+}
+
+const actionsIds = {
+  setName: 'setname',
+  setLastname: 'setlastname',
+};
+
+// action creator:
+const setLastname = (lastname: string): Action => ({
+  type: actionsIds.setLastname,
+  payload: lastname,
+});
+
+const setName = (setName: string): Action => ({
+  type: actionsIds.setName,
+  payload: name,
+});
+
+const userInfoReducer = (state: UserState, action: Action): UserState => {
+  switch(action.type)
+  {
+    case actionsIds.setName:
+      return {
+       ...state,
+       name: action.payload,
+      };
+    case actionsIds.setLastname:
+      return {
+        ...state,
+        lastname: action.payload,
+      };
+  }
+  return state;
+}
+
+interface Props {
+  name: string;
+  onChange: (value: string) => void;
+}
+
+const EditUsername: React.FC<Props> = React.memo((props) => {
+  console.log(
+    "Hey I'm only rerendered when name gets updated, check React.memo"
+  );
+
+  return (
+    <input
+      value={props.name}
+      onChange={(e) =>
+        props.dispatch({type: actionsIds.setLastname, payload: e.target.value})
+      }
+    />
+  );
+});
+
+export const MyComponent = () => {
+  const [userInfo, dispatch] = React.useReducer(userInfoReducer,
+     {
+       name: 'John',
+       lastname: 'Doe',
+      });
 
   return (
     <>
-      <h4>{myName}</h4>
-      <input value={myName} onChange={(e) => setMyName(e.target.value)} />
+      <h3>
+        {userInfo.name} {userInfo.lastname}
+      </h3>
+      <EditUsername
+        name={userInfo.name}
+        onChange={(name) =>
+          setInfo({
+            ...userInfo,
+            name,
+          })
+        }
+      />
+      <input
+        value={userInfo.lastname}
+        onChange={(e) =>
+          dispatch({type: actionsIds.setLastname, payload: e.target.value})
+        }
+      />
     </>
   );
 };
