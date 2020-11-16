45a5f5de2218a7ecac0c9641d62e6b9917b0d8eb
Ramirez David <david.ramirez@tui.be>
2020-11-16 11:46:19 +0100

    Reset button using hook useCallback

--- 03-hooks/01-use-state/src/demo.tsx
+++ 03-hooks/01-use-state/src/demo.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 
-export const MyComponent: React.FC = () => {
-  const [myName, setMyName] = React.useState("John Doe");
+interface Props {
+  onReset: () => void;
+}
+
+const ResetValue: React.FC<Props> = React.memo((props) => {
+  console.log(
+    "Hey I'am only rendered the first time, check React.memo + callback"
+  );
+
+  return <button onClick={props.onReset}>Reset value</button>;
+});
+
+export const MyComponent = () => {
+  const [username, setUsername] = React.useState("John");
+  const [lastname, setLastname] = React.useState("Doe");
+
+  //useCallback carga la función cuando se carga el DOM
+  // y la cachea, así que no la repinta cada vez que se
+  // llama al componente.
+  const resetNameCallback = React.useCallback(() => {
+    setUsername("");
+  }, []);
 
   return (
     <>
-      <h4>{myName}</h4>
-      <input value={myName} onChange={(e) => setMyName(e.target.value)} />
+      <h3>
+        {username} {lastname}
+      </h3>
+      <input value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input value={lastname} onChange={(e) => setLastname(e.target.value)} />
+      <ResetValue onReset={resetNameCallback}>Reset name</ResetValue>
     </>
   );
 };
